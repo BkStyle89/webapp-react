80264cd031f2de079f70d4829c45a8720f34a866
BkStyle89 <kajzer@hotmail.it>
2026-04-21 15:39:20 +0200

    making reviews page

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,7 +1,19 @@
+import { useEffect,useState } from "react";
+import axios from 'axios';
 import AppHeader from "../components/AppHeader";
 import AppFooter from "../components/AppFooter";
 export default function FilmPage(){
 
+const review_key=import.meta.env.VITE_REVIEW_KEY
+const[reviews,setreviews] =useState([]);
+  
+useEffect(() => {
+  axios.get(`${review_key}`)
+  .then(response=> {
+  setFilms(response.data);
+})
+  .catch(error => console.error(error));
+},[]);
 
   return (
     <>
